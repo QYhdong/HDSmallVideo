d4d641275985ecd33461e082f03e7dcec14a370c
qingyun <809184372@qq.com>
2017-08-07 12:00:37 +0800

    create

---
+++ HDSmallVideo/HDSmallVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HDSmallVideo
+//
+//  Created by ddd on 17/8/7.
+//  Copyright © 2017年 HuangDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HDSmallVideo/HDSmallVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HDSmallVideo
+//
+//  Created by ddd on 17/8/7.
+//  Copyright © 2017年 HuangDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
