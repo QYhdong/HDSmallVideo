a1353d1e0817aab0f0c564e308dbd33065498b4b
QYhdong <809184372@qq.com>
2018-04-16 19:18:25 +0800

    first add

---
+++ HDSmallVideo/HDSmallVideo/HdSmallVideo/HDVideoController.h
@@ -0,0 +1,13 @@
+//
+//  HDVideoController.h
+//  HDSmallVideo
+//
+//  Created by HJddd on 2018/4/16.
+//  Copyright © 2018年 HuangDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HDVideoController : UIViewController
+
+@end
