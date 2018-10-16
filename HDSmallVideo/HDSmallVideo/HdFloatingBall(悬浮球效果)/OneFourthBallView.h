3ffd818351dacb79d3a658d1f5e6716561310f1a
QYhdong <809184372@qq.com>
2018-10-16 19:35:00 +0800

    提交代码

---
+++ HDSmallVideo/HDSmallVideo/HdFloatingBall(悬浮球效果)/OneFourthBallView.h
@@ -0,0 +1,22 @@
+//
+//  OneFourthBallView.h
+//  HDSmallVideo
+//
+//  Created by HJddd on 2018/10/16.
+//  Copyright © 2018年 HuangDong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OneFourthBallView : UIView
+
+//悬浮球是否正在显示
+-(void)isShowingOneFourthView:(BOOL)showing;
+
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HDSmallVideo/HDSmallVideo/HdFloatingBall(悬浮球效果)/OneFourthBallView.m
@@ -0,0 +1,32 @@
+//
+//  OneFourthBallView.m
+//  HDSmallVideo
+//
+//  Created by HJddd on 2018/10/16.
+//  Copyright © 2018年 HuangDong. All rights reserved.
+//
+
+#import "OneFourthBallView.h"
+
+@implementation OneFourthBallView
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self){
+        self.backgroundColor = [UIColor clearColor];
+        [self setupUI];
+    }
+    return self;
+}
+
+-(void)setupUI{
+    
+}
+
+//是否正在显示
+-(void)isShowingOneFourthView:(BOOL)showing{
+    
+    
+}
+
+@end
