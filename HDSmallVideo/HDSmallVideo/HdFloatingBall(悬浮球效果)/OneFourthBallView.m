a80f51d6ce37dda4d6807a27f973ef66e946527e
QYhdong <809184372@qq.com>
2018-10-17 13:47:03 +0800

    add info

--- HDSmallVideo/HDSmallVideo/HdFloatingBall(悬浮球效果)/OneFourthBallView.m
+++ HDSmallVideo/HDSmallVideo/HdFloatingBall(悬浮球效果)/OneFourthBallView.m
@@ -7,8 +7,22 @@
 //
 
 #import "OneFourthBallView.h"
+#import "UIView+FloatFrame.h"
+
+@interface OneFourthBallView()
+
+//显示文字
+@property (nonatomic, strong) UILabel *label;
+
+@property (nonatomic, strong) UIImageView *imageView;
+
+//四分之一圆模糊板
+@property (nonatomic,strong) UIToolbar *toolbar;
+
+@end
 
 @implementation OneFourthBallView
+
 
 -(instancetype)initWithFrame:(CGRect)frame{
     self = [super initWithFrame:frame];
@@ -21,6 +35,31 @@
 
 -(void)setupUI{
     
+    _toolbar = [UIToolbar new];
+    _toolbar.barStyle = UIBarStyleBlack;
+    [self addSubview: _toolbar];
+    _imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"FloatBallResource.bundle/demo_cancel_float_default"]];
+    _imageView.contentMode = UIViewContentModeCenter;
+    [self addSubview:_imageView];
+    _label = [UILabel new];
+    _label.font = [UIFont systemFontOfSize:10];
+    _label.textColor = [UIColor colorWithHue:0 saturation:0 brightness:0.9 alpha:1.0];
+    _label.textAlignment = NSTextAlignmentCenter;
+    _label.text = @"浮窗";
+    [self addSubview:_label];
+    
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    _toolbar.frame = self.bounds;
+    _imageView.size = CGSizeMake(40, 40);
+    _imageView.centerX = self.width / 2.0 + 26;
+    _imageView.centerY = self.height / 2.0;
+    _label.size = CGSizeMake(self.width, 20);
+    _label.centerX = _imageView.centerX;
+    _label.y = CGRectGetMaxY(_imageView.frame) + 14;
 }
 
 //是否正在显示
